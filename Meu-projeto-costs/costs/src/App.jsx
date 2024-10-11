011164f82a4f23725e54f24fc777ba841fbb02bf
Fabio Conceição <fabio.conceicaoads@gmail.com>
2024-10-11 09:38:44 -0300

    Segunda parte projeto
    estruturação das páginas

--- Meu-projeto-costs/costs/src/App.jsx
+++ Meu-projeto-costs/costs/src/App.jsx
@@ -1,15 +1,31 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './Components/pages/Home'
+import Company from './Components/pages/Company'
+import Contact from './Components/pages/Contact'
+import NewProject from './Components/pages/NewProject'
 
+import Container from './Components/layout/Container'
 
 function App() {
-
-
   return (
-    <>
-    <div className='App' >
-      <p>Costs</p>
-    </div>
-     
-    </>
+    <Router>
+      <div>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Empresa</Link>
+        <Link to='/newproject'>Novo Projeto</Link>
+      </div>
+      <Container customClass='min-height'>
+        <Routes>        
+            <Route exact path='/' element={<Home />} />
+            <Route path='/company' element={<Company />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+      <p>Footer</p>
+    </Router>
+  
   )
 }
 
