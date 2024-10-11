5f7e4ed1ffd81d6624522089405fa85d6f5628c2
Fabio Conceição <fabio.conceicaoads@gmail.com>
2024-10-11 18:32:02 -0300

    Criação header e footer

--- Meu-projeto-costs/costs/src/App.jsx
+++ Meu-projeto-costs/costs/src/App.jsx
@@ -1,31 +1,29 @@
 import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
 import Home from './Components/pages/Home'
+import Project from './Components/pages/Projects'
 import Company from './Components/pages/Company'
 import Contact from './Components/pages/Contact'
 import NewProject from './Components/pages/NewProject'
 
 import Container from './Components/layout/Container'
+import Navbar from './Components/layout/NavBar'
+import Footer from './Components/layout/Footer'
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to='/'>Home</Link>
-        <Link to='/contact'>Contato</Link>
-        <Link to='/company'>Empresa</Link>
-        <Link to='/newproject'>Novo Projeto</Link>
-      </div>
+      <Navbar />
       <Container customClass='min-height'>
         <Routes>        
             <Route exact path='/' element={<Home />} />
+            <Route path='/project' element={<Project />} />
             <Route path='/company' element={<Company />} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/newproject' element={<NewProject />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer />
     </Router>
-  
   )
 }
 
---
+++ Meu-projeto-costs/costs/src/Components/pages/Projects.jsx
@@ -0,0 +1,7 @@
+function Project() {
+    return(
+        <h1>Projetos</h1>
+    )
+}
+
+export default Project
