3226a82a10ebb7209423f4ed0e3e8a135aefd415
Fabio Conceição <fabio.conceicaoads@gmail.com>
2024-10-12 17:27:11 -0300

    Terceira etapa do proejto
    criação da pagina de formulario dividida em componentes

---
+++ Meu-projeto-costs/costs/src/Components/project/ProjectForm.jsx
@@ -0,0 +1,28 @@
+import Input from '../form/input'
+import Select from '../form/Select'
+import Submit from '../form/Submit'
+import styles from './ProjectForm.module.css'
+
+
+function ProjectFrom({btnText}) {
+    return(
+        <form className={styles.form}>
+            <Input 
+                type='text' 
+                text='Nome do projeto' 
+                name='name'
+                placeholder='Insira o nome do projeto'
+            />
+            <Input 
+                type='number' 
+                text='Orçamento do projeto' 
+                name='budget'
+                placeholder='Insira o orçamento total'
+            />
+            <Select name='category_id' text='Selecione a categoria' />
+            <Submit text={btnText}/>
+        </form>
+    )
+}
+
+export default ProjectFrom
