f2b7a2ab13690d6ffbfa6d2530b3f6caee48f057
Fabio Conceição <fabio.conceicaoads@gmail.com>
2024-10-14 21:54:45 -0300

    Criação API
    Criação API com db.json (sumila uma API/Banco de dados)

--- Meu-projeto-costs/costs/src/Components/project/ProjectForm.jsx
+++ Meu-projeto-costs/costs/src/Components/project/ProjectForm.jsx
@@ -1,3 +1,5 @@
+import {useEffect, useState} from 'react'
+
 import Input from '../form/input'
 import Select from '../form/Select'
 import Submit from '../form/Submit'
@@ -5,6 +7,23 @@ import styles from './ProjectForm.module.css'
 
 
 function ProjectFrom({btnText}) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/jason'
+            }
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return(
         <form className={styles.form}>
             <Input 
@@ -19,7 +38,11 @@ function ProjectFrom({btnText}) {
                 name='budget'
                 placeholder='Insira o orçamento total'
             />
-            <Select name='category_id' text='Selecione a categoria' />
+            <Select 
+                name='category_id' 
+                text='Selecione a categoria'
+                options={categories}  
+            />
             <Submit text={btnText}/>
         </form>
     )
