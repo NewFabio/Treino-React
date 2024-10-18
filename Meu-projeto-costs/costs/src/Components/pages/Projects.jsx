318a5f56b76180b447c4cc750bc49e9c5dac1878
Fabio Conceição <fabio.conceicaoads@gmail.com>
2024-10-18 20:56:02 -0300

    Inclusao icons, estilização

--- Meu-projeto-costs/costs/src/Components/pages/Projects.jsx
+++ Meu-projeto-costs/costs/src/Components/pages/Projects.jsx
@@ -1,8 +1,16 @@
 import Message from '../layout/Message'
 
 import { useLocation } from 'react-router-dom'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+import ProjectCard from '../project/ProjectCard'
+
+import styles from './Projects.module.css'
+import { useState, useEffect } from 'react'
 
 function Project() {
+
+    const [project, setProject] = useState([])
 
     const location = useLocation()
     let message = ''
@@ -10,10 +18,40 @@ function Project() {
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json',
+            },
+        })
+        .then(resp => resp.json())
+        .then(data => {
+            console.log(data)
+            setProject(data)
+        })
+        .catch((err) => console.log(err))
+    }, [])
+
     return(
-        <div>
-            <h1>Meus Projetos</h1>
+        <div className={styles.project_container} >
+            <div className={styles.title_container} >
+                <h1>Meus Projetos</h1>
+                <LinkButton to='/newproject' text='Criar Projeto' />
+            </div>
             {message && <Message type='success' msg={message} />}
+            <Container customClass='start' >
+                {project.length > 0 &&
+                    project.map((project) => (
+                        <ProjectCard 
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                            key={project.id}
+                        />
+                    ))}
+            </Container>
         </div>
     )
 }
